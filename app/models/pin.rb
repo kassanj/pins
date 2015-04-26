0e77e283c969230f82d4157fd34a137ba22eecdb
Kassandra M <kassandram022@gmail.com>
2015-04-26 18:41:26 -0400

    added figaro and dropbox gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,9 @@ class Pin < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments
-
+  
   has_attached_file :image, :styles => { :medium => "300x300>" }
+            :storage => :dropbox,
+            :dropbox_credentials => Rails.root.join("config/dropbox.yml")
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
